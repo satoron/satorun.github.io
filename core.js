e337ebf2b84a4a2a6fbbac5b737a524012c6ebaf
satorun <itsfinetoday1@gmail.com>
2014-11-25 13:09:38 +0900

    20141125

--- core.js
+++ core.js
@@ -7,9 +7,7 @@ function convert(in_text, tab_type){
 	var tab_code = "";
 	var tab_level = 0;
 	var literal_start = false;
-	var ppdfn_start = false; 
-	var pstack = [];
-	var rmv_list = [];
+	var ppdfn_count = 0; 
 	var tab_level= 0;
 	var literal_start = false;
 
@@ -21,8 +19,6 @@ function convert(in_text, tab_type){
 
 	for (var i = 0; i < arr_text.length; i++) {
 		var chr = arr_text[i];
-		console.log("token = " + token);
-		console.log("out_text = " + out_text);
 		if (literal_start){
 			switch (chr){
 				case '"':
@@ -91,9 +87,6 @@ function convert(in_text, tab_type){
 								out_text += "\n" + tab_code.repeat(2)
 								 + operator_kw + chr;
 								break;
-							case "AND":
-							case "OR":
-								pstack.pop;
 							default:
 								out_text += operator_kw + chr;
 								break;
@@ -110,21 +103,20 @@ function convert(in_text, tab_type){
 					break;
 				case "(":
 					if (ppdfn_kw.indexOf(token.toUpperCase() >= 0)){
-						ppdfn_start = true;
+						ppdfn_count++;
 					}
-					pstack.push(i);
 					out_text += token + chr;
 					token = "";
 					break;
 				case ")":
-					if (pstack.length > 0){
-						rmv_list.push(pstack[pstack.length-1]);
-						pstack.pop();
-						rmv_list.push(i);
+					if (ppdfn_count){
+						ppdfn_count--;
 					}
+					out_text += token + chr;
+					token = "";
 					break;
 				case ",":
-					if (ppdfn_start){
+					if (ppdfn_count){
 						out_text += token + chr;
 					}else{
 						out_text += token + "\n"
@@ -144,8 +136,108 @@ function convert(in_text, tab_type){
 			}
 		}
 	};
+	out_text = parentheses_remove(out_text);
+	out_text = parentheses_break(out_text, tab_code);
 
 	return out_text;
+};
+
+/*
+function parentheses_remove(text, rmv_list){
+	var rtn_text = "";
+	var arr_text = text.split("");
+	for (var i = 0; i < arr_text.length; i++){
+		if (rmv_list.indexOf(i) >= 0) {
+			continue;
+		}
+		rtn_text += arr_text[i];
+	}
+	return rtn_text;
+};
+*/
+
+function parentheses_remove(text){
+	var arr_text = text.split("");
+	var rtn_text = "";
+	var pstack = [];
+	var rmv_list = [];
+	var token = "";
+	for (var i = 0; i < arr_text.length; i++){
+		switch (arr_text[i]){
+			case "(":
+				pstack.push(i);
+				break;
+			case ")":
+				if (pstack.length > 0){
+					rmv_list.push(pstack[pstack.length-1]);
+					pstack.pop();
+					rmv_list.push(i);
+				}
+				break;
+			case " ":
+				if (token == "AND" || token == "OR" || token == "FROM"){
+					pstack.pop();
+				}
+				token = "";	
+				break;
+			default:
+				token += arr_text[i];
+		}
+	};
+	for (var i = 0; i < arr_text.length; i++){
+		if (rmv_list.indexOf(i) >= 0) {
+			continue;
+		}
+		rtn_text += arr_text[i];
+	}
+
+	return rtn_text;
+};
+
+function parentheses_break(text, tab_code){
+	var rtn_text = "";
+	var tab_level = 0;
+	var token = "";
+	var ppdfn_start = false;
+
+	for (var i = 0; i < text.length; i++){
+		var chr = text[i];
+		switch (chr){
+			case "(":
+				if (ppdfn_kw.indexOf(token) >= 0){
+					ppdfn_start = true;
+					rtn_text += chr;
+				}else{
+					tab_level++;
+					rtn_text += chr + "\n" + tab_code.repeat(tab_level);
+					if (text[i + 1] == "\n")i++
+				}
+				token = "";
+				break;
+			case ")":
+				if (ppdfn_start){
+					ppdfn_start = false;
+					rtn_text += chr;
+				}else{
+					tab_level--;
+					rtn_text += "\n" + tab_code.repeat(tab_level) + chr;
+				}
+				token = "";
+				break;
+			case " ":
+				token = "";
+				rtn_text += chr;
+				break;
+			case "\n":
+				token = "";
+				rtn_text += chr + tab_code.repeat(tab_level);
+				break;
+			default:
+				token += chr;
+				rtn_text += chr;
+		}
+	}
+	return rtn_text;
 };
 
 String.prototype.repeat = function(num) {
